079a857b6742526a763621e0e6a7bcd6d3b3b90e
Jack Song <jacktator@users.noreply.github.com>
2019-01-04 01:03:47 +1100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php
 
 add_action('wp_enqueue_scripts', 'ch_theme_enqueue_styles');
-
 function ch_theme_enqueue_styles() {
 
 	$parent_style = 'creditcard-parent-style';
@@ -12,6 +11,23 @@ function ch_theme_enqueue_styles() {
 		get_stylesheet_directory_uri() . '/style.css',
 		array($parent_style)
 	);
+}
+
+/*
+    Fixes WP5.0 & WPBakery Page Builder loses Role Settings upon 
+    
+    You can set the post type for which the editor should be
+    available by adding the following code to functions.php:
+    
+    Author: Jacktator
+    Plugin: WPBakery Page Builder 5.6
+    Reference: https://stackoverflow.com/questions/49316654/wp-bakery-page-builder-loses-settings-in-role-manager?rq=1
+*/
+add_action( 'vc_before_init', 'Use_wpBakery' );
+function Use_wpBakery() {
+  $vc_list = array('page','capabilities','add_custom_post_type_here');
+  vc_set_default_editor_post_types($vc_list);
+  vc_editor_set_post_types($vc_list);
 }
 
 ?>
