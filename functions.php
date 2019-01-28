583d978d7a89b18339b99a6b7fc72a362259b470
Jack Song <133342301@163.com>
2019-01-29 09:44:45 +1100

    Templatizing Credit Card Custom Post Type

--- functions.php
+++ functions.php
@@ -1156,7 +1156,7 @@ function ch_generate_apply_now_button($atts) {
 				$button_html .= '<a href="' . $link_link . '" class="sc_button color_style_default sc_button_default sc_button_size_normal sc_button_icon_left"><span class="sc_button_text"><span class="sc_button_title">Apply Now^</span></span></a>';
 			}
 
-			echo $button_html;
+			return $button_html;
 
 		}
 	}
