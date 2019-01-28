53acfb0ba7186219aa58317316f1f3226690393c
Jack Song <133342301@163.com>
2019-01-29 09:42:35 +1100

    Templatizing Credit Card Custom Post Type

--- functions.php
+++ functions.php
@@ -1185,7 +1185,7 @@ function ch_generate_compare_button($atts) {
 		'format_value' => true, // Default
 	), $atts));
 
-	return '<a href="#" class="sc_button color_style_default sc_button_default sc_button_size_normal sc_button_icon_left alike-button alike-button-style" data-post-id="' . $post_id . '" data-post-title="' . get_the_title() . '" data-post-thumb="' . get_the_post_thumbnail_url() . '" data-post-link="' . get_post_link() . '" title="Add To Compare">
+	echo '<a href="#" class="sc_button color_style_default sc_button_default sc_button_size_normal sc_button_icon_left alike-button alike-button-style" data-post-id="' . $post_id . '" data-post-title="' . get_the_title() . '" data-post-thumb="' . get_the_post_thumbnail_url() . '" data-post-link="' . get_post_link() . '" title="Add To Compare">
 Add To Compare</a>';
 
 }
